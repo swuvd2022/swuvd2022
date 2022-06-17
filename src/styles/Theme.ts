321a00843e8360b25233ef68d544fd923d739e04
gytks4 <gytks4@naver.com>
2022-06-18 08:48:29 +0900

    chore: DefaultTheme 수정

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,10 +1,8 @@
-import { DefaultTheme } from 'styled-components';
-
 export const enum size {
   tablet = 768,
 }
 
-const theme: DefaultTheme = {
+const theme = {
   brandColor_1: '#5A5FE8',
   brandColor_2: '#E0E0E0',
   brandColor_3: '#FBFDFF',
