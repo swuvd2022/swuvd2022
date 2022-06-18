3c4d569552a9d74ca414e563131284c35b9b7862
gytks4 <gytks4@naver.com>
2022-06-18 09:44:57 +0900

    feat: html, body, root에 height 100% 적용

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -11,8 +11,22 @@ export const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     color: inherit;
   }
+
+  html {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+
   body {
     font-family: Noto Sans CJK KR, sans-serif;
     letter-spacing: -0.03px; 
+  }
+
+  #root, body {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    height: 100%;
   }
 `;
