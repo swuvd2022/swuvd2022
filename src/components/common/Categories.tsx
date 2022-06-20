c0fb1bc59666cf178ed602d6ec8b36d0d8eff995
gytks4 <gytks4@naver.com>
2022-06-20 23:29:52 +0900

    feat: 카테고리 컴포넌트 구현

---
+++ src/components/common/Categories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface CategoriesProps {
+  categories: readonly string[];
+  category: string;
+  setCategory: React.Dispatch<React.SetStateAction<string>>;
+  flexDirection: 'column' | 'row';
+  margin: string;
+}
+
+const Categories = ({
+  categories,
+  category,
+  setCategory,
+  flexDirection,
+  margin,
+}: CategoriesProps) => {
+  return (
+    <StyledCategories flexDirection={flexDirection} margin={margin}>
+      {categories.map(item => (
+        <StyledCategory key={item} onClick={() => setCategory(item)} active={item === category}>
+          {item}
+        </StyledCategory>
+      ))}
+    </StyledCategories>
+  );
+};
+
+export default Categories;
+
+const StyledCategories = styled.ul<{ flexDirection: 'column' | 'row'; margin: string }>`
+  display: flex;
+  flex-direction: ${({ flexDirection }) => flexDirection};
+  margin: ${({ margin }) => margin};
+  gap: 30px;
+  word-break: keep-all;
+`;
+
+const StyledCategory = styled.li<{ active: boolean }>`
+  color: ${({ active, theme }) => (active ? theme.brandColor_1 : theme.brandColor_2)};
+  font-size: 24px;
+  font-weight: 700;
+  cursor: pointer;
+`;
